7b12e21a074c3626a2260522665f326db238dcb4
NancyGomez <nagomez@csumb.edu>
2018-02-27 22:35:09 -0800

    Added Log In screen, no functionality

---
+++ parse_chat/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  parse_chat
+//
+//  Created by Nancy Gomez on 2/27/18.
+//  Copyright © 2018 Nancy Gomez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onLogIn(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
